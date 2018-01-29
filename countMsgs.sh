04bfba3be8aa81318e35fbb492188ea6e4113b5d
Abdelhamide EL ARIB <abdelhamide.elarib@ebiznext.com>
2018-01-29 10:51:23 +0100

    add countMsgs

--- countMsgs.sh
+++ countMsgs.sh
@@ -2,9 +2,10 @@
 
 source $(dirname $0)/config.sh
 
-if [ "$am_i_cool" = true ]; then
-
-    $_KAFKA_BIN/kafka-run-class kafka.tools.GetOffsetShell --broker-list $_KAFKA_BOOTSTRAP_SERVERS --topic $1  --time -1 | awk -F  ":" '{sum += $3} END {print sum}'
-
+#Check arguments
+if [[ -z "${1}" ]]; then
+    echo "Must provide the name of topic to count as first argument "
+    exit 1
 fi
 
+$_KAFKA_BIN/kafka-run-class kafka.tools.GetOffsetShell --broker-list $_KAFKA_BOOTSTRAP_SERVERS --topic $1  --time -1 | awk -F  ":" '{sum += $3} END {print sum}'
